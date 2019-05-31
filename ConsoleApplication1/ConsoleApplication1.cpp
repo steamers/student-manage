946c88ea17fd7b4c95b8342c01da5268e51e4f66
Hmoji <945619928@qq.com>
2019-06-01 01:42:50 +0800

    1.0

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,9 +2,9 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-typedef struct st{
+typedef struct st {
 	char name[5];
-	int Cgrade, Mgrade, Egrade;
+	int Cgrade, Mgrade, Egrade, total;
 	st *next;
 }st;
 struct pack {
@@ -17,80 +17,73 @@ struct pack {
 	char MMAXname[5], MMINname[5];
 	int EMAX = 0, EMIN = INT_MAX;
 	char EMAXname[5], EMINname[5];
-	st *head;
+	st *head = NULL;
 };
-struct pack *p=(struct pack*)malloc(sizeof(struct pack));
-st *end=p->head;
-st *work = end;
-FILE *fp ;
+//struct pack *p = (struct pack*)malloc(sizeof(struct pack));
+struct pack p = { 0,0,INT_MAX,"0","0",0,INT_MAX,"0","0",0,INT_MAX,"0","0",0,INT_MAX,"0","0",NULL };
+st *end = p.head;
 void addst() {
-	fp = fopen("C:\\Users\\Administrator\\Desktop\\student.txt", "a+");
-	if (fp == NULL)
-		exit(1);
-	char n='y';
-	while (n!='n') {
-		end = (st*)malloc(sizeof(st));
+	char n = 'y';
+	while (n != 'n') {
+		p.num == 0 ? p.head = (st*)malloc(sizeof(st)) : end = (st*)malloc(sizeof(st));
 		puts("请输入学生姓名：");
 		scanf("%s", &end->name);
-		fprintf(fp, "%s", end->name);
 		puts("请输入语文成绩：");
 		scanf("%d", &end->Cgrade);
-		fprintf(fp, "%d", end->Cgrade);
-		if (end->Cgrade > p->MAX) {
-			p->MAX = end->Cgrade;
-			strcpy(p->MAXname, end->name);
+		if (end->Cgrade > p.MAX) {
+			p.MAX = end->Cgrade;
+			strcpy(p.MAXname, end->name);
 		}
-		if (end->Cgrade < p->MIN) {
-			p->MIN = end->Cgrade;
-			strcpy(p->MINname, end->name);
+		if (end->Cgrade < p.MIN) {
+			p.MIN = end->Cgrade;
+			strcpy(p.MINname, end->name);
 		}
-		if (end->Cgrade > p->CMAX) {
-			p->CMAX = end->Cgrade;
-			strcpy(p->CMAXname, end->name);
+		if (end->Cgrade > p.CMAX) {
+			p.CMAX = end->Cgrade;
+			strcpy(p.CMAXname, end->name);
 		}
-		if (end->Cgrade < p->CMIN) {
-			p->CMIN = end->Cgrade;
-			strcpy(p->CMINname, end->name);
+		if (end->Cgrade < p.CMIN) {
+			p.CMIN = end->Cgrade;
+			strcpy(p.CMINname, end->name);
 		}
 		puts("请输入数学成绩：");
 		scanf("%d", &end->Mgrade);
-		fprintf(fp, "%d", end->Mgrade);
-		if (end->Mgrade > p->MAX) {
-			p->MAX = end->Mgrade;
-			strcpy(p->MAXname, end->name);
+		if (end->Mgrade > p.MAX) {
+			p.MAX = end->Mgrade;
+			strcpy(p.MAXname, end->name);
 		}
-		if (end->Mgrade < p->MIN) {
-			p->MIN = end->Mgrade;
-			strcpy(p->MINname, end->name);
+		if (end->Mgrade < p.MIN) {
+			p.MIN = end->Mgrade;
+			strcpy(p.MINname, end->name);
 		}
-		if (end->Mgrade > p->MMAX) {
-			p->MMAX = end->Mgrade;
-			strcpy(p->MMAXname, end->name);
+		if (end->Mgrade > p.MMAX) {
+			p.MMAX = end->Mgrade;
+			strcpy(p.MMAXname, end->name);
 		}
-		if (end->Mgrade < p->MMIN) {
-			p->MMIN = end->Mgrade;
-			strcpy(p->MMINname, end->name);
+		if (end->Mgrade < p.MMIN) {
+			p.MMIN = end->Mgrade;
+			strcpy(p.MMINname, end->name);
 		}
 		puts("请输入英语成绩：");
 		scanf("%d", &end->Egrade);
-		fprintf(fp, "%d", end->Egrade);
-		if (end->Egrade > p->MAX) {
-			p->MAX = end->Egrade;
-			strcpy(p->MAXname, end->name);
+		if (end->Egrade > p.MAX) {
+			p.MAX = end->Egrade;
+			strcpy(p.MAXname, end->name);
 		}
-		if (end->Egrade < p->MIN) {
-			p->MIN = end->Egrade;
-			strcpy(p->MINname, end->name);
+		if (end->Egrade < p.MIN) {
+			p.MIN = end->Egrade;
+			strcpy(p.MINname, end->name);
 		}
-		if (end->Egrade > p->EMAX) {
-			p->EMAX = end->Egrade;
-			strcpy(p->EMAXname, end->name);
+		if (end->Egrade > p.EMAX) {
+			p.EMAX = end->Egrade;
+			strcpy(p.EMAXname, end->name);
 		}
-		if (end->Egrade < p->EMIN) {
-			p->EMIN = end->Egrade;
-			strcpy(p->EMINname, end->name);
+		if (end->Egrade < p.EMIN) {
+			p.EMIN = end->Egrade;
+			strcpy(p.EMINname, end->name);
 		}
-		p->num++;
+		p.num++;
+		end->total = end->Cgrade + end->Egrade + end->Mgrade;
 		end = end->next;
 		printf("继续输入？y/n\n");
 		getchar();
@@ -98,12 +91,44 @@ void addst() {
 		system("cls");
 	}
 }
+void sortUI() {
+	int i = 1;
+	if (p.num == 0) {
+		while (i) {
+			printf("抱歉！目前学生数据为空！\n按0退出。");
+			scanf("%d", &i);
+		}
+		return;
+	}
+	while (1) {
+		printf("*****************************************\n");
+		printf("*   1.按总分          2.按语文成绩      *\n");
+		printf("*   3.按数学成绩      4.按英语成绩      *\n");
+		printf("*                     0.退出            *\n");
+		printf("*****************************************\n");
+		int n;
+		printf("请输入功能序号：");
+		scanf("%d", &n);
+		system("cls");
+		switch (n) {
+		case 1:;
+		case 2:;
+		case 3:;
+		case 4:;
+		case 0:return;
+		}
+	}
+}
+void tsort() {
+	st *work = p.head;
+
+}
 int main() {
 	while (1) {
 		printf("*****************************************\n");
 		printf("*   1.添加学生          2.分数统计      *\n");
 		printf("*   3.成绩排序          4.查找学生      *\n");
-		printf("*   5.保存              0.退出          *\n");
+		printf("*                       0.退出          *\n");
 		printf("*****************************************\n");
 		int n;
 		printf("请输入功能序号：");
@@ -112,7 +137,7 @@ int main() {
 		switch (n) {
 		case 1:addst(); break;
 		case 2:break;
-		case 3:break;
+		case 3:sortUI(); break;
 		case 4:break;
 		case 5:break;
 		case 0:return 0;
