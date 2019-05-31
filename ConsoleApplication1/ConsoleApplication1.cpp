5765d63f38b4f5ba5a6b627761e7b662b7f2ee24
Hmoji <945619928@qq.com>
2019-06-01 04:51:46 +0800

    添加排序功能

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,31 +3,238 @@
 #include<string.h>
 #include<stdlib.h>
 typedef struct st {
-	char name[5];
+	char name[10];
 	int Cgrade, Mgrade, Egrade, total;
 	st *next;
 }st;
 struct pack {
-	int num = 0;
-	int MAX = 0, MIN = INT_MAX;
+	int num;
+	int MAX, MIN;
 	char MAXname[5], MINname[5];
-	int CMAX = 0, CMIN = INT_MAX;
+	int CMAX, CMIN;
 	char CMAXname[5], CMINname[5];
-	int MMAX = 0, MMIN = INT_MAX;
+	int MMAX, MMIN;
 	char MMAXname[5], MMINname[5];
-	int EMAX = 0, EMIN = INT_MAX;
+	int EMAX, EMIN;
 	char EMAXname[5], EMINname[5];
-	st *head = NULL;
+	st *next;
 };
-//struct pack *p = (struct pack*)malloc(sizeof(struct pack));
 struct pack p = { 0,0,INT_MAX,"0","0",0,INT_MAX,"0","0",0,INT_MAX,"0","0",0,INT_MAX,"0","0",NULL };
-st *end = p.head;
+st *end;
+st *endpr;
+void HTL(st *p) {
+	if (p->next != NULL) {
+		HTL(p = p->next);
+	}
+	printf("%s  %d  %d  %d  %d\n", p->name, p->total, p->Cgrade, p->Mgrade, p->Egrade);
+}
+void LTH() {
+	st *q = p.next->next;
+	while (q != NULL) {
+		printf("%s  %d  %d  %d  %d\n", q->name, q->total, q->Cgrade, q->Mgrade, q->Egrade);
+		q = q->next;
+	}
+}
+void tsort() {
+	end->next= (st*)malloc(sizeof(st));
+	end = end->next;
+	st *tra = p.next->next;
+	st *trapr = p.next;
+	st *mark;
+	st *markpr;
+	st *foot = end;
+	int n = INT_MAX;
+	while (p.next->next != foot) {
+		while (tra != foot) {
+			if (tra->total <= n) {
+				n = tra->total;
+				mark = tra;
+				markpr = trapr;
+				trapr = tra;
+				tra = tra->next;
+			}
+			else {
+				trapr = tra;
+				tra = tra->next;
+			}
+		}
+		markpr->next = mark->next;
+		end->next = mark;
+		end = mark;
+		mark->next = NULL;
+		tra = p.next->next;
+		trapr = p.next;
+		n = INT_MAX;
+	}
+	p.next->next = foot->next;
+	free(foot);
+	int m;
+	printf("*****************************************\n");
+	printf("*   1.由高到低排序                      *\n");
+	printf("*   2.由低到高排序                      *\n");
+	printf("*                     0.退出            *\n");
+	printf("*****************************************\n");
+	printf("请输入功能序号：");
+	scanf("%d", &m);
+	system("cls");
+	switch (m) {
+	case 1:HTL(p.next->next); break;
+	case 2:LTH(); break;
+	case 0:return;
+	}
+}
+void Csort() {
+	end->next = (st*)malloc(sizeof(st));
+	end = end->next;
+	st *tra = p.next->next;
+	st *trapr = p.next;
+	st *mark;
+	st *markpr;
+	st *foot = end;
+	int n = INT_MAX;
+	while (p.next->next != foot) {
+		while (tra != foot) {
+			if (tra->Cgrade <= n) {
+				n = tra->Cgrade;
+				mark = tra;
+				markpr = trapr;
+				trapr = tra;
+				tra = tra->next;
+			}
+			else {
+				trapr = tra;
+				tra = tra->next;
+			}
+		}
+		markpr->next = mark->next;
+		end->next = mark;
+		end = mark;
+		mark->next = NULL;
+		tra = p.next->next;
+		trapr = p.next;
+		n = INT_MAX;
+	}
+	p.next->next = foot->next;
+	free(foot);
+	int m;
+	printf("*****************************************\n");
+	printf("*   1.由高到低排序                      *\n");
+	printf("*   2.由低到高排序                      *\n");
+	printf("*                     0.退出            *\n");
+	printf("*****************************************\n");
+	printf("请输入功能序号：");
+	scanf("%d", &m);
+	system("cls");
+	switch (m) {
+	case 1:HTL(p.next->next); break;
+	case 2:LTH(); break;
+	case 0:return;
+	}
+}
+void Msort() {
+	end->next = (st*)malloc(sizeof(st));
+	end = end->next;
+	st *tra = p.next->next;
+	st *trapr = p.next;
+	st *mark;
+	st *markpr;
+	st *foot = end;
+	int n = INT_MAX;
+	while (p.next->next != foot) {
+		while (tra != foot) {
+			if (tra->Mgrade <= n) {
+				n = tra->Mgrade;
+				mark = tra;
+				markpr = trapr;
+				trapr = tra;
+				tra = tra->next;
+			}
+			else {
+				trapr = tra;
+				tra = tra->next;
+			}
+		}
+		markpr->next = mark->next;
+		end->next = mark;
+		end = mark;
+		mark->next = NULL;
+		tra = p.next->next;
+		trapr = p.next;
+		n = INT_MAX;
+	}
+	p.next->next = foot->next;
+	free(foot);
+	int m;
+	printf("*****************************************\n");
+	printf("*   1.由高到低排序                      *\n");
+	printf("*   2.由低到高排序                      *\n");
+	printf("*                     0.退出            *\n");
+	printf("*****************************************\n");
+	printf("请输入功能序号：");
+	scanf("%d", &m);
+	system("cls");
+	switch (m) {
+	case 1:HTL(p.next->next); break;
+	case 2:LTH(); break;
+	case 0:return;
+	}
+}
+void Esort() {
+	end->next = (st*)malloc(sizeof(st));
+	end = end->next;
+	st *tra = p.next->next;
+	st *trapr = p.next;
+	st *mark;
+	st *markpr;
+	st *foot = end;
+	int n = INT_MAX;
+	while (p.next->next != foot) {
+		while (tra != foot) {
+			if (tra->Egrade <= n) {
+				n = tra->Egrade;
+				mark = tra;
+				markpr = trapr;
+				trapr = tra;
+				tra = tra->next;
+			}
+			else {
+				trapr = tra;
+				tra = tra->next;
+			}
+		}
+		markpr->next = mark->next;
+		end->next = mark;
+		end = mark;
+		mark->next = NULL;
+		tra = p.next->next;
+		trapr = p.next;
+		n = INT_MAX;
+	}
+	p.next->next = foot->next;
+	free(foot);
+	int m;
+	printf("*****************************************\n");
+	printf("*   1.由高到低排序                      *\n");
+	printf("*   2.由低到高排序                      *\n");
+	printf("*                     0.退出            *\n");
+	printf("*****************************************\n");
+	printf("请输入功能序号：");
+	scanf("%d", &m);
+	system("cls");
+	switch (m) {
+	case 1:HTL(p.next->next); break;
+	case 2:LTH(); break;
+	case 0:return;
+	}
+}
 void addst() {
 	char n = 'y';
 	while (n != 'n') {
-		p.num == 0 ? p.head = (st*)malloc(sizeof(st)) : end = (st*)malloc(sizeof(st));
+		end->next = (st*)malloc(sizeof(st));
+		end = end->next;
+		end->next = NULL;
 		puts("请输入学生姓名：");
-		scanf("%s", &end->name);
+		scanf("%s", end->name);
 		puts("请输入语文成绩：");
 		scanf("%d", &end->Cgrade);
 		if (end->Cgrade > p.MAX) {
@@ -84,10 +291,10 @@ void addst() {
 		}
 		p.num++;
 		end->total = end->Cgrade + end->Egrade + end->Mgrade;
-		end = end->next;
 		printf("继续输入？y/n\n");
 		getchar();
 		scanf("%c", &n);
+		getchar();
 		system("cls");
 	}
 }
@@ -111,19 +318,17 @@ void sortUI() {
 		scanf("%d", &n);
 		system("cls");
 		switch (n) {
-		case 1:;
-		case 2:;
-		case 3:;
-		case 4:;
+		case 1:tsort(); break;
+		case 2:Csort(); break;
+		case 3:Msort(); break;
+		case 4:Esort(); break;
 		case 0:return;
 		}
 	}
 }
-void tsort() {
-	st *work = p.head;
-
-}
 int main() {
+	p.next = (st*)malloc(sizeof(st));//建一个空的头结点
+	end = p.next;
 	while (1) {
 		printf("*****************************************\n");
 		printf("*   1.添加学生          2.分数统计      *\n");
